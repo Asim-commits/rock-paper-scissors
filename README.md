bce18eac10cc6960f9fedc37ea7d33afe786da47
Asim-commits <asim.baral1717@gmail.com>
2024-02-12 21:25:27 +0545

    Final Changes to TOP Project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rock-paper-scissors
+
+Checklist 
+
+a) Play the game from browser console
+b) Implement the solution using problem-solving technique
+
--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,65 +1,90 @@
 
-function playGame(rounds) {
-    function playRound(playerSelection, computerSelection) {
+let roundResult = document.querySelector(".round-results");
+let score = document.querySelector(".scores");
+let finalResult = document.querySelector(".final-result");
+const button = document.querySelectorAll('.button');
+const rock = button[0];
+const paper = button[1];
+const scissors = button[2];
 
-        if (playerSelection === computerSelection) {
-            return "0";
-        }
+// Stores the number of rounds user played, won and lost.
+let count = 1;
+let userWin = 0;
+let computerWin = 0;
 
-        /* The computer will beat user when: 
-        a) Computer choice: Rock User choice: Scissors
-        b) Computer choice: Paper User choice: Rock
-        c) Computer choice: Scissor User choice: Paper */
-        if ((playerSelection == "scissors" && computerSelection == "rock") || (playerSelection == "rock" && computerSelection =="paper") || (playerSelection == "paper" && computerSelection == "scissors")) {
-            return "loose";
-        } else {
-            return "win";
-        }
+score.innerText = `Player: ${userWin} Computer: ${computerWin}`;
+
+// The function is called each time the button is pressed except when the user has played 5 rounds.
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
+        roundResult.innerText = "Tie! Play the game again";
+        return;
     }
 
-    function getComputerChoice() {
-        let choices = ["Rock", "Paper", "Scissors"];
-        let randomNumber = Math.floor(Math.random() * 3);
-        return choices[randomNumber].toLowerCase();
-    }
-
-    /* The lines that are commented are additional features which i will add later on. */
-    // function decideWinner(playerWin, computerWin) {
-    //     if (playerWin > computerWin) {
-    //         console.log(playerWin, computerWin);
-    //         console.log("Congratulations!!!! You won");
-    //     } else {
-    //         console.log("You lose. Better luck next time");
-    //     }
-    // }
-
-    const playerSelection = prompt("Rock, Paper or Scissors").toLowerCase();
-    const computerSelection = getComputerChoice();
-    let playerWin = 0;
-    let computerWin = 0;
-
-    resultOfTheRound = playRound(playerSelection, computerSelection);
-
-    if (resultOfTheRound == "0") {
-        console.log("Tie! Play the game again");
-        playGame(rounds);
+    /* The computer will beat user when: 
+    a) Computer choice: Rock User choice: Scissors
+    b) Computer choice: Paper User choice: Rock
+    c) Computer choice: Scissor User choice: Paper */
+    if ((playerSelection == "scissors" && computerSelection == "rock") || (playerSelection == "rock" && computerSelection =="paper") || (playerSelection == "paper" && computerSelection == "scissors")) {
+        roundResult.innerText = `Round:  ${count}
+                            You played: ${playerSelection} 
+                            Computer played: ${computerSelection} 
+                            You Lost!`;
+        computerWin++;
+        count++;
     } else {
-        if (resultOfTheRound == "win") {
-            console.log("Round: " + rounds + " You Win! " + playerSelection + " beats " + computerSelection);
-            // playerWin++;
-        } else {
-            console.log("Round: " + rounds + " You Lose! " + computerSelection + " beats " + playerSelection);
-            // computerWin++;
-        }
+        roundResult.innerText = `Round:  ${count} 
+                            You played: ${playerSelection} 
+                            Computer played: ${computerSelection} 
+                            You won!`;
+        
+        userWin++;
+        count++;
     }
 
-    // if (rounds == 5) {
-    //     decideWinner(playerWin, computerWin);
-    // }
+    score.innerText = `Player: ${userWin} Computer: ${computerWin}`;
+
+
+    // Disable the buttons after the user has played 5 rounds and at the same time announce the winner by using another function.
+    if (count > 5) {
+        let allButtons = document.getElementsByClassName("button");
+
+        for (let i = 0; i < allButtons.length; i++) {
+            allButtons[i].disabled = true
+        }
+
+        roundResult.style.display = "none"; 
+
+        decideWinner(userWin, computerWin);
+    } 
 }
 
-
-
-for (let i = 1; i <= 5; i++) {
-    playGame(i);
+function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"];
+    let randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber].toLowerCase();
 }
+
+function decideWinner(userWin, computerWin) {    
+    if (userWin > computerWin) {
+        finalResult.innerText = `Congratualtions!!!! You won!`;
+    } else {
+        finalResult.innerText = `Computer won! Better luck next time.`;
+    }
+}
+
+rock.addEventListener("click", () => {
+    let computerChoice = getComputerChoice();
+    playRound("rock", computerChoice);
+});
+
+paper.addEventListener("click", () => {
+    let computerChoice = getComputerChoice();
+    playRound("paper", computerChoice);
+});
+
+scissors.addEventListener("click", () => {
+    let computerChoice = getComputerChoice();
+    playRound("scissors", computerChoice);
+});
